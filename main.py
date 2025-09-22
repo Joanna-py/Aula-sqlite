73e426479f20dd1c237b1a7eb702bb8f10819556
Joanna Santos <joanna.d.souza@aluno.senai.br>
2025-09-22 10:21:45 -0300

    função de update

--- main.py
+++ main.py
@@ -9,46 +9,61 @@ conexao = sqlite3.connect("escola.db")
 
 cursor = conexao.cursor()
 
-#Criando uma tabela no banco chamada de "alunos"
+# #Criando uma tabela no banco chamada de "alunos"
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER,
-    curso TEXT                      
-    )             
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS alunos (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     idade INTEGER,
+#     curso TEXT                      
+#     )             
                
                
-""")
-print("Tabela criada com sucesso!")
+# """)
+# print("Tabela criada com sucesso!")
 
 
 
-#Inserindo um valor no banco de dados (aluno)
+# #Inserindo um valor no banco de dados (aluno)
+
+# cursor.execute("""
+# INSERT INTO alunos (nome, idade, curso)
+# VALUES (?, ?, ?)                          
+# """, 
+# ("Laura", 24, "Engenharia")
+# )
+
+# lista_alunos = [
+#     ("Raul", 20, "Direito"),
+#     ("Vinicius", 31, "Computação"),
+#     ("Vitoria", 78, "medicina"),
+# ]
+
+# #Executemany permite inserir múltiplas linhas
+# cursor.executemany("""
+# INSERT INTO alunos (nome, idade, curso)
+# VALUES (?, ?, ?)                          
+# """,
+# lista_alunos )
+
+
+# #Confirmar as alterações do banco
+
+# conexao.commit()
+# print("Dados inseridos com sucesso!")
+
+#Atualizar os dados da tabela alunos
+#Atualizar o nome de um aluno
+#Sempre usar WHERE, senão todos os registros serão alterados!
 
 cursor.execute("""
-INSERT INTO alunos (nome, idade, curso)
-VALUES (?, ?, ?)                          
-""", 
-("Laura", 24, "Engenharia")
+UPDATE alunos
+SET nome = ?
+WHERE id = ?
+""", ("Felipe", "ADS", 2)
+
 )
 
-lista_alunos = [
-    ("Raul", 20, "Direito"),
-    ("Vinicius", 31, "Computação"),
-    ("Vitoria", 78, "medicina"),
-]
-
-#Executemany permite inserir múltiplas linhas
-cursor.executemany("""
-INSERT INTO alunos (nome, idade, curso)
-VALUES (?, ?, ?)                          
-""",
-lista_alunos )
-
-
-#Confirmar as alterações do banco
-
 conexao.commit()
-print("Dados inseridos com sucesso!")
+print("Dados atualizados com sucesso!")
